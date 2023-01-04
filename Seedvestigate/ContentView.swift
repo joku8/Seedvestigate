5eb4a150b4ebc0e9851ee7ce0b055415b7b9c050
Joseph Ku <josephku825@gmail.com>
2023-01-04 16:22:46 -0600

    Initial Commit

---
+++ Seedvestigate/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Seedvestigate
+//
+//  Created by Joseph Ku on 1/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Seedvestigate/SeedvestigateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeedvestigateApp.swift
+//  Seedvestigate
+//
+//  Created by Joseph Ku on 1/4/23.
+//
+
+import SwiftUI
+
+@main
+struct SeedvestigateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
